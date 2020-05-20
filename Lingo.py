432278765b267cda77fb97d300583251f6d156fc
calebalex-ander <12Graff15$>
2020-05-19 21:20:28 -0500

    Added multiple player capability

--- Lingo.py
+++ Lingo.py
@@ -10,19 +10,17 @@ import random
 import sys
 
 
-def check_endgame(word, guess, tries):
+def check_if_right(word, guess, tries):
     if guess == word:
-        replay = input(f"Congratulations, you've won! The word was: {word}. Play again? (y/n) ").lower()
+        print(f"Congratulations, you've won! The word was: {word}.")
+        input("Press enter to continue...")
+        return 1
     elif tries == 5:
-        replay = input(f"I'm sorry, you lost. The word was: {word}. Play again? (y/n) ").lower()
+        print(f"I'm sorry, you lost. The word was: {word}.")
+        input("Press enter to continue...")
+        return 0
     else:
         return 0
-    if replay == "y":
-        print("Starting new game...")
-        main()
-    else:
-        print("Thanks for playing!!")
-        sys.exit()
 
 
 def get_word():
@@ -43,7 +41,7 @@ def update_word(solution, indices): #  Updates displayed word containing correct
     return word
 
 
-def initialize_lingo_board():
+def initialize_lingo_board():  # Not currently implemented; initializes bingo board
     nums_found = False
     num_list = []
     board = []
@@ -74,13 +72,14 @@ def display_board(board):  # Not currently implemented; displays bingo board
 
 def display_guesses(guesses):
     for guess in range(1, len(guesses)):
+        print(f"Guess {guess}: ", end=" ")
         display_word(guesses[guess])
         print()
 
 
 def display_word(word):
     for letter in range(len(word)):
-        print(f"{word[letter]}", end="\t")
+        print(f"\t{word[letter]}", end="\t")
 
 
 def check_validity(guess, word):
@@ -100,30 +99,69 @@ def check_validity(guess, word):
     return indices_correct
 
 
-def play_game(word, board):
-    print(f"Welcome to Lingo.py. The object of this game is to guess a 5-letter word in 5 tries.")
-    print(f"After each guess, you are told which letters are in the correct position, which aren't, and which are just wrong.")
-    guess_allowance = 5
-    tries = 0
-    guess_so_far = word[0:2] + "---"
-    tot_indices_correct = [0, 1]
-    guesses = [word, guess_so_far]
-    for i in range(guess_allowance):
-        display_guesses(guesses)
-        guess = input("\nHere's your word. Make a guess! ").lower()
-        guesses[i+1] = guess
-        cur_indices_correct = check_validity(guess, word)
-        tot_indices_correct.extend(cur_indices_correct)
-        guesses.append(update_word(word, tot_indices_correct))
-        tries += 1
-        check_endgame(word, guesses[i+1], tries)
-        input("Press enter to continue...")
+def initialize_scores(players):
+    scores = []
+    for i in range(len(players)):
+        scores.append(0)
+    return scores
+
+
+def initialize_players():
+    num_players = int(input("How many players will there be? "))
+    players = []
+    for player in range(num_players):
+        players.append(input(f"Please give a nickname for player {player + 1}: "))
+    return players
+
+
+def show_scoreboard(players, scores):
+    print("Current Standings:")
+    for i in range(len(players)):
+        print(f"{players[i]}: {scores[i]}")
+    print()
+
+
+def play_game():
+
+    board = initialize_lingo_board()
+    players = initialize_players()
+    scoreboard = initialize_scores(players)
+    player_up = 0
+    winning_score = 1
+
+    while True:
+        word = get_word()
+        show_scoreboard(players, scoreboard)
+        print(f"Okay {players[player_up]}, it's your turn!")
+        guess_allowance = 5
+        tries = 0
+        guess_so_far = word[0:2] + "---"
+        tot_indices_correct = [0, 1]
+        guesses = [word, guess_so_far]
+        for i in range(guess_allowance):
+            display_guesses(guesses)
+            guess = input("\nHere's your word. Make a guess! ").lower()
+            guesses[i+1] = guess
+            cur_indices_correct = check_validity(guess, word)
+            tot_indices_correct.extend(cur_indices_correct)
+            guesses.append(update_word(word, tot_indices_correct))
+            tries += 1
+            if check_if_right(word, guesses[i+1], tries):
+                scoreboard[player_up] += 1
+                break
+        if winning_score in scoreboard:
+            print(f"Congratulations, {players[scoreboard.index(winning_score)]}, you've won!")
+            sys.exit()
+        else:
+            player_up += 1
+            if player_up == len(players):
+                player_up = 0
 
 
 def main():
-    word = get_word()
-    board = initialize_lingo_board()
-    play_game(word, board)
+    print(f"Welcome to Lingo.py. The object of this game is to guess a 5-letter word in 5 tries.")
+    print(f"After each guess, you are told which letters are in the correct position, which aren't, and which are just wrong.")
+    play_game()
     return 0
 
 
